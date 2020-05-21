ef8e76b7d3104d0c2478dc8d2af6ee6c032e6da1
liuwanyong2017 <2674706698@qq.com>
2020-05-21 19:24:23 +0800

    实现class Axios的基本方法

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -1,6 +1,48 @@
-
+import {AxiosPromise, RequestConfig} from "../types";
+import {dispatchRequest} from "./dispatchRequest";
 
 
 export class Axios {
+  request(config: RequestConfig): AxiosPromise {
+    return dispatchRequest(config);
+  }
 
+  _processDispatchRequestWithoutData(url: string, method: string, config?: RequestConfig):AxiosPromise {
+    return dispatchRequest(Object.assign(config || {url}, {url, method}));
+  }
+
+  get(url: string, config?: RequestConfig): AxiosPromise {
+    return this._processDispatchRequestWithoutData(url, "get", config);
+  }
+
+  delete(url: string, config?: RequestConfig): AxiosPromise {
+    return this._processDispatchRequestWithoutData(url, "delete", config);
+  }
+
+  head(url: string, config?: RequestConfig): AxiosPromise {
+    return this._processDispatchRequestWithoutData(url, "head", config);
+  }
+
+  options(url: string, config?: RequestConfig): AxiosPromise {
+    return this._processDispatchRequestWithoutData(url, "options", config);
+  }
+
+  _processDispatchRequestWithData(url:string,method:string,data?:any,config?:RequestConfig):AxiosPromise {
+    return dispatchRequest(Object.assign(config || {url}, {url, method,data}));
+  }
+  post(url:string,data?:any,config?:RequestConfig):AxiosPromise{
+    return this._processDispatchRequestWithData(
+      url,'post',data,config
+    )
+  }
+  put(url:string,data?:any,config?:RequestConfig):AxiosPromise{
+    return this._processDispatchRequestWithData(
+      url,'put',data,config
+    )
+  }
+  patch(url:string,data?:any,config?:RequestConfig):AxiosPromise{
+    return this._processDispatchRequestWithData(
+      url,'patch',data,config
+    )
+  }
 }
---
+++ src/core/dispatchRequest.ts
@@ -0,0 +1,39 @@
+import {AxiosPromise, RequestConfig} from "../types";
+import xml from "../core/xml";
+import makeURL from "../helpers/url";
+import transformReqData, {transformResData} from "../helpers/data";
+import processHeaders from "../helpers/headers";
+
+export const dispatchRequest = (config: RequestConfig): AxiosPromise => {
+  processConfig(config);
+  return xml(config).then(
+    res => {
+      res.data = transformResponseData(res.data);
+      return res;
+    }
+  );
+};
+
+function processConfig(config: RequestConfig) {
+  config.url = transformURL(config);
+  config.headers = transformHeaders(config);
+  config.data = transformRequireData(config);
+}
+
+function transformURL({url, params}: RequestConfig): string {
+  return makeURL(url!, params);
+}
+
+function transformRequireData({data}: RequestConfig) {
+  return transformReqData(data);
+}
+
+function transformHeaders({headers = {}, data}: RequestConfig) {
+  return processHeaders(headers, data);
+}
+
+function transformResponseData(data: any): any {
+  return transformResData(data);
+}
+
+
